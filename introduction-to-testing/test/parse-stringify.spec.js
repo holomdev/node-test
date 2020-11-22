8004ef22cc46529383b4ea9729018523cd31c5ab
holomdev <holom.dev@gmail.com>
2020-11-22 17:46:15 -0600

    testing parse and stringify functions

---
+++ introduction-to-testing/test/parse-stringify.spec.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const { parse, stringify } = require('../parse-stringify');
+
+describe('The stringify function', () => {
+  it('should stringify an object into valid query string', function () {
+    const actual = stringify({ by: 'kati-frantz'});
+    const expected = 'by=kati-frantz';
+    assert.strictEqual(actual, expected);
+  });
+})
+
+describe('The parse function', () => {
+  it('should parse a query string into an object', function () {
+    const actual = parse('?by=kati-frantz');
+    const expected = { by: 'kati-frantz' };
+    assert.deepStrictEqual(actual, expected);
+  });
+})
