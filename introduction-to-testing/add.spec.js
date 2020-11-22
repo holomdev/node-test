c1face8ecc46a8cf4c73471333cb48648de78971
holomdev <holom.dev@gmail.com>
2020-11-22 17:06:26 -0600

    what are test runners

--- introduction-to-testing/add.spec.js
+++
@@ -1,7 +0,0 @@
-const assert = require('assert');
-const add = require('./add');
-
-const result = add(1, 3);
-const expected = 4;
-
-assert.strictEqual(result, expected, 'Add function does not work properly');
