ed2ac024e1007a89adb8ac2efbeb36a7e9b8ba39
holomdev <holom.dev@gmail.com>
2020-11-23 08:32:38 -0600

    Migrating to jest

--- introduction-to-testing/test/users.spec.js
+++ introduction-to-testing/test/users.spec.js
@@ -1,45 +1,37 @@
-const assert = require('assert');
 const {findUserById, findUserByEmail} = require('../users');
 
 describe('The findUserByEmail function', () => {
   it('should find user by email ', (done) => {
     findUserByEmail('bahdcoder@gmail.com').then((response) => {
-      assert.strictEqual(response.message, 'User found successfully.');
+      expect(response.message).toBe('User found successfully.')
       done();
     });
   });
 
   it('finds a user by email (using the return promise method)', () => {
     return findUserByEmail('bahdcoder@gmail.com').then((response) => {
-      assert.strictEqual(response.message, 'User found successfully.');
+      expect(response.message).toBe('User found successfully.')
     })
   });
 
   it('finds a user by email (Using async/await)', async () => {
     const response = await findUserByEmail('bahdcoder@gmail.com');
-    assert.strictEqual(response.message, 'User found successfully.');
+    expect(response.message).toBe('User found successfully.');
   });
 
-  it('rejects with errors if user with email was not found', () => {
-    return findUserByEmail('x@y.com').then((result) => {
-      assert.fail('Expected findUserByEmail function to reject.')
-    }, (error) => {
-      assert.strictEqual(error.message, 'User with email: x@y.com was not found.')
-    })
+  it('rejects with errors if user with email was not found', async () => {
+    await expect(findUserByEmail('x@y.com')).rejects.toEqual(new Error('User with email: x@y.com was not found.'));
+
   });
 })
 
 describe('The findUserById function', () => {
   it('find a user by ID (Using async/await)', async () => {
     const response = await findUserById(1);
-    assert.strictEqual(response.message, 'User found successfully.');
+    expect(response.message).toBe('User found successfully.');
   })
 
-  it('should reject if user is not found by ID',  () => {
-    return findUserById(0).then((result) => {
-      assert.fail('Expected findUserById function to reject.');
-    }, (error) => {
-      assert.strictEqual(error.message, 'User with id: 0 was not found.');
-    })
+  it('rejects with errors if user with id was not found', async () => {
+    await expect(findUserById(0)).rejects.toEqual(new Error('User with id: 0 was not found.'));
   });
 })
